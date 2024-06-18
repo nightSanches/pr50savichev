66ee4d1baba3f8499842cbd2ee20ffae3fc28fe4
nightSanches <galesavich@gmail.com>
2024-06-18 09:35:04 +0500

    Разработанный пользовательский элемент собственника. Савичев

---
+++ Elements/Owner.xaml.cs
@@ -0,0 +1,30 @@
+﻿using pr50savichev.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pr50savichev.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для Owner.xaml
+    /// </summary>
+    public partial class Owner : UserControl
+    {
+        public Owner(OwnerContext roomOwner)
+        {
+            InitializeComponent();
+            NameOwner.Content = $"{roomOwner.LastName} {roomOwner.FirstName} {roomOwner.SurName}";
+        }
+    }
+}
